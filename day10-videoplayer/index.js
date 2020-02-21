a4596379aefb8fa4625c8080e8992bcf1ff4f783
jameshelou <james_christien@hotmail.com>
2020-02-21 16:47:34 +1100

    add day10-videoplayer

---
+++ day10-videoplayer/index.js
@@ -0,0 +1,80 @@
+// Get elements
+const player = document.querySelector('.player');
+const video = player.querySelector('video');
+const timeline = player.querySelector('.timeline');
+const progress = player.querySelector('.progress');
+const playButton = player.querySelector('.playButton');
+const speedButton = player.querySelector('.speedButton');
+const volumeSlider = player.querySelector('.volumeSlider');
+const supportedSpeed = [1, 1.25, 1.5, 1.75, 2, 0.25, 0.5, 0.75];
+let currSpeed = 0;
+
+// Video player functions
+function playVideo() {
+    if (video.paused) {
+        video.play();
+        playButton.innerHTML = '<i class="material-icons">pause</i>';
+    } else {
+        video.pause();
+        playButton.innerHTML = '<i class="material-icons">play_arrow</i>';
+    }
+}
+
+function skipVideo(e) {
+    switch (e.keyCode) {
+        case 32:
+            playVideo();
+            break;
+        case 37:
+            video.currentTime -= 5;
+            break;
+        case 39:
+            video.currentTime += 5;
+            break;
+        default:
+            return;
+    }
+}
+
+function handleSliderChange() {
+    video[this.name] = this.value;
+}
+
+function handleSpeedChange() {
+    currSpeed += 1;
+    video[this.name] = supportedSpeed[(currSpeed) % supportedSpeed.length];
+    speedButton.textContent = supportedSpeed[(currSpeed) % supportedSpeed.length];
+}
+
+function handleProgressChange() {
+    progress.style.width = `${(video.currentTime / video.duration) * 100}%`;
+}
+
+function handleScrub(e) {
+    const percent = (e.offsetX / timeline.offsetWidth) * video.duration;
+    video.currentTime = percent;
+}
+
+function handleDragScrub() {
+    this.onmousemove = e => {
+        handleScrub(e)
+    }
+}
+
+function cancelDragScrub() {
+    timeline.onmousemove = null;
+}
+
+
+// Hook up listeners
+video.addEventListener('click', playVideo);
+playButton.addEventListener('click', playVideo);
+volumeSlider.addEventListener('input', handleSliderChange);
+speedButton.addEventListener('click', handleSpeedChange);
+video.addEventListener('timeupdate', handleProgressChange);
+timeline.addEventListener('click', handleScrub);
+timeline.addEventListener('mousedown', handleDragScrub);
+document.addEventListener('mouseup', cancelDragScrub);
+
+// video keyboard controls
+document.addEventListener('keydown', skipVideo);
